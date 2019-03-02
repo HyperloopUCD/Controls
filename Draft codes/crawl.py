79fcdadde25f820e5d9f17d16ec06f08966cdcac
Brandon Lau <41304817+brandon-lau0@users.noreply.github.com>
2019-03-02 12:42:25 -0800

    Add files via upload
    updated crawl() function

--- Draft codes/crawl.py
+++ Draft codes/crawl.py
@@ -1,17 +1,17 @@
 CUR_POD_DISTANCE = 0  # in centimeters
 CRAWL_SPEED = 223.52  # in centimeters / second
 END_DISTANCE = 123000  # in centimeters
-CRAWL_POWER = 50
-
-def movePodWithMotor(power):
-	pwmControl(power)
+CRAWL_POWER = 50	
 
 def slowCrawl(CRAWL_POWER):
-  while CUR_POD_DISTANCE < END_DISTANCE:
-    movePodWithMotor(CRAWL_POWER)
+  pwmControl(power)
+
 
 def crawl():
-  slowCrawl(CRAWL_SPEED)
-  brake()
+  if CUR_POD_DISTANCE < END_DISTANCE:
+    slowCrawl(CRAWL_POWER)
+  else:
+  	slowCrawl(0)  # Stop the motors
+  	cmd = 5
 
 crawl()
